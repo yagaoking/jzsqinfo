750ca6cbe795ca5cc32fbbb0622911c2f38ea9cc
db <yagaoking>
2020-02-17 17:08:00 +0800

    template tag not complete

---
+++ blog/templatetags/blog_extras.py
@@ -0,0 +1,10 @@
+from django import template
+from ..models import Post, Category, Tag
+
+register = template.Library()
+
+@register.inclusion_tag('blog/inclusions/_recent_posts.html',takes_context=True)
+def show_recent_posts(context, num=5):
+    return {
+        'recent_post_list': Post.objects.all().order_by('-created_time')[:num],
+    }
