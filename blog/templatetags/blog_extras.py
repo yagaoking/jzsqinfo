764199177e17769f0805c0aa93cca8ab69b883b3
db <yagaoking>
2020-02-21 13:55:56 +0800

    model tag(finished)

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -9,7 +9,7 @@ register = template.Library()
 @register.inclusion_tag('blog/inclusions/_recent_posts.html',takes_context=True)
 def show_recent_posts(context, num=5):
     return {
-        'recent_post_list': Post.objects.all().order_by('-created_time')[:num],
+        'recent_post_list': Post.objects.all().order_by('-create_time')[:num],
     }
 
 # 归档模板标签
@@ -18,7 +18,7 @@ def show_recent_posts(context, num=5):
 @register.inclusion_tag('blog/inclusions/_archives.html', takes_context=True)
 def show_archives(context):
     return {
-        'date_list': Post.objects.dates('created_time', 'month', order='DESC'),
+        'date_list': Post.objects.dates('create_time', 'month', order='DESC'),
     }
 
 # 分类模板标签
@@ -28,4 +28,13 @@ def show_archives(context):
 def show_categories(context):
     return {
         'category_list': Category.objects.all(),
+    }
+
+# 标签云模板标签
+
+
+@register.inclusion_tag('blog/inclusions/_tags.html', takes_context=True)
+def show_tags(context):
+    return {
+        'tag_list': Tag.objects.all(),
     }
